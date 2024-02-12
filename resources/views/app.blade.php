33cd9eb15976ef4a66ea1e0f095879ee69136b03
Jun Alcantara <junalcantara.dev@gmail.com>
2024-02-12 12:59:59 +0800

    Chat plugin

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,12 +7,20 @@
     @vite('resources/css/app.css')
     <script src="https://kit.fontawesome.com/661c1762f6.js" crossorigin="anonymous"></script>
     @inertiaHead
-    <div id="fb-root"></div>
-    <div id="fb-customer-chat" class="fb-customerchat"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId            : '1368287130495002',
+          xfbml            : true,
+          version          : 'v19.0'
+        });
+      };
+    </script>
+    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
   </head>
   <body>
     @inertia
-    <script>
+    {{-- <script>
       var chatbox = document.getElementById('fb-customer-chat');
       chatbox.setAttribute("page_id", "61556529386413");
       chatbox.setAttribute("attribution", "biz_inbox");
@@ -34,6 +42,10 @@
         js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
         fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));
-    </script>
+    </script> --}}
+
+    
+    <div id="fb-root"></div>
+    <div id="fb-customer-chat" class="fb-customerchat"></div>
   </body>
 </html>
